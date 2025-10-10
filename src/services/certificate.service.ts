ee715e4674cac33b95fde417d923632784c496dc
Thunggg <thuannguyen20041028@gmail.com>
2025-10-10 18:05:38 +0700

    optimize: remove save file on local

--- src/services/certificate.service.ts
+++ src/services/certificate.service.ts
@@ -1,10 +1,6 @@
 import { createHash } from 'crypto'
-import path from 'path'
-import { TYPE_CERTIFICATE } from '~/constants/type-certificate'
 import { uploadMetadataToCloudinary, uploadToCloudinary } from '~/helpers/cloudinary'
-import { generateERC721Metadata } from '~/helpers/generate-metadata'
 import { BadRequestError } from '~/ultis/CustomErrors'
-import { saveFile } from '~/ultis/file'
 import { addWatermark } from '~/ultis/Watermark'
 import { ethers } from 'ethers'
 
@@ -29,14 +25,9 @@ export const mintCertificateService = async ({ owner, file }: MintParams) => {
   // 2) Watermark + lưu file vào public/uploads
   const watermarkedBuffer = await addWatermark(file.buffer, file.mimetype)
 
-  const ext = file.mimetype === 'application/pdf' ? '.pdf' : file.mimetype === 'image/png' ? '.png' : '.jpg'
-  const outName = `${fileHashHex}_wm${ext}`
-  const outPath = path.resolve(process.cwd(), 'public', 'uploads', outName)
-
   // 3) Upload Cloudinary
   const [fileUrl] = await Promise.all([
-    uploadToCloudinary(watermarkedBuffer as Buffer, 'certificates', resourceType, fileHashHex),
-    saveFile(outPath, watermarkedBuffer as Buffer)
+    uploadToCloudinary(watermarkedBuffer as Buffer, 'certificates', resourceType, fileHashHex)
   ])
 
   // 4) Tạo metadata JSON
@@ -56,7 +47,7 @@ export const mintCertificateService = async ({ owner, file }: MintParams) => {
       { trait_type: 'type', value: 'certificate' }
     ],
     // Sau này làm trang verify thì thêm vào
-    external_url: ""
+    external_url: ''
   }
 
   // 5) Upload metadata JSON vào Cloudinary
@@ -67,22 +58,306 @@ export const mintCertificateService = async ({ owner, file }: MintParams) => {
   const privateKey = process.env.PRIVATE_KEY
   const contractAddress = process.env.CERTIFICATE_CONTRACT_ADDRESS
 
-  const contractABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"address","name":"owner","type":"address"}],"name":"ERC721IncorrectOwner","type":"error"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ERC721InsufficientApproval","type":"error"},{"inputs":[{"internalType":"address","name":"approver","type":"address"}],"name":"ERC721InvalidApprover","type":"error"},{"inputs":[{"internalType":"address","name":"operator","type":"address"}],"name":"ERC721InvalidOperator","type":"error"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"ERC721InvalidOwner","type":"error"},{"inputs":[{"internalType":"address","name":"receiver","type":"address"}],"name":"ERC721InvalidReceiver","type":"error"},{"inputs":[{"internalType":"address","name":"sender","type":"address"}],"name":"ERC721InvalidSender","type":"error"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ERC721NonexistentToken","type":"error"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"OwnableInvalidOwner","type":"error"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"OwnableUnauthorizedAccount","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"_fromTokenId","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"_toTokenId","type":"uint256"}],"name":"BatchMetadataUpdate","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"},{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":false,"internalType":"bytes32","name":"fileHash","type":"bytes32"},{"indexed":false,"internalType":"string","name":"tokenURI","type":"string"}],"name":"CertificateMinted","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"MetadataUpdate","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"fileHash","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_owner","type":"address"},{"internalType":"bytes32","name":"_fileHash","type":"bytes32"},{"internalType":"string","name":"_tokenURI","type":"string"}],"name":"mintCertificate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"nextTokenId","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"bytes","name":"","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"bool","name":"","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes32","name":"_fileHash","type":"bytes32"}],"name":"verifyHash","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}]
-
+  const contractABI = [
+    { inputs: [], stateMutability: 'nonpayable', type: 'constructor' },
+    {
+      inputs: [
+        { internalType: 'address', name: 'sender', type: 'address' },
+        { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+        { internalType: 'address', name: 'owner', type: 'address' }
+      ],
+      name: 'ERC721IncorrectOwner',
+      type: 'error'
+    },
+    {
+      inputs: [
+        { internalType: 'address', name: 'operator', type: 'address' },
+        { internalType: 'uint256', name: 'tokenId', type: 'uint256' }
+      ],
+      name: 'ERC721InsufficientApproval',
+      type: 'error'
+    },
+    {
+      inputs: [{ internalType: 'address', name: 'approver', type: 'address' }],
+      name: 'ERC721InvalidApprover',
+      type: 'error'
+    },
+    {
+      inputs: [{ internalType: 'address', name: 'operator', type: 'address' }],
+      name: 'ERC721InvalidOperator',
+      type: 'error'
+    },
+    {
+      inputs: [{ internalType: 'address', name: 'owner', type: 'address' }],
+      name: 'ERC721InvalidOwner',
+      type: 'error'
+    },
+    {
+      inputs: [{ internalType: 'address', name: 'receiver', type: 'address' }],
+      name: 'ERC721InvalidReceiver',
+      type: 'error'
+    },
+    {
+      inputs: [{ internalType: 'address', name: 'sender', type: 'address' }],
+      name: 'ERC721InvalidSender',
+      type: 'error'
+    },
+    {
+      inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+      name: 'ERC721NonexistentToken',
+      type: 'error'
+    },
+    {
+      inputs: [{ internalType: 'address', name: 'owner', type: 'address' }],
+      name: 'OwnableInvalidOwner',
+      type: 'error'
+    },
+    {
+      inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
+      name: 'OwnableUnauthorizedAccount',
+      type: 'error'
+    },
+    {
+      anonymous: false,
+      inputs: [
+        { indexed: true, internalType: 'address', name: 'owner', type: 'address' },
+        { indexed: true, internalType: 'address', name: 'approved', type: 'address' },
+        { indexed: true, internalType: 'uint256', name: 'tokenId', type: 'uint256' }
+      ],
+      name: 'Approval',
+      type: 'event'
+    },
+    {
+      anonymous: false,
+      inputs: [
+        { indexed: true, internalType: 'address', name: 'owner', type: 'address' },
+        { indexed: true, internalType: 'address', name: 'operator', type: 'address' },
+        { indexed: false, internalType: 'bool', name: 'approved', type: 'bool' }
+      ],
+      name: 'ApprovalForAll',
+      type: 'event'
+    },
+    {
+      anonymous: false,
+      inputs: [
+        { indexed: false, internalType: 'uint256', name: '_fromTokenId', type: 'uint256' },
+        { indexed: false, internalType: 'uint256', name: '_toTokenId', type: 'uint256' }
+      ],
+      name: 'BatchMetadataUpdate',
+      type: 'event'
+    },
+    {
+      anonymous: false,
+      inputs: [
+        { indexed: true, internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+        { indexed: true, internalType: 'address', name: 'owner', type: 'address' },
+        { indexed: false, internalType: 'bytes32', name: 'fileHash', type: 'bytes32' },
+        { indexed: false, internalType: 'string', name: 'tokenURI', type: 'string' }
+      ],
+      name: 'CertificateMinted',
+      type: 'event'
+    },
+    {
+      anonymous: false,
+      inputs: [{ indexed: false, internalType: 'uint256', name: '_tokenId', type: 'uint256' }],
+      name: 'MetadataUpdate',
+      type: 'event'
+    },
+    {
+      anonymous: false,
+      inputs: [
+        { indexed: true, internalType: 'address', name: 'previousOwner', type: 'address' },
+        { indexed: true, internalType: 'address', name: 'newOwner', type: 'address' }
+      ],
+      name: 'OwnershipTransferred',
+      type: 'event'
+    },
+    {
+      anonymous: false,
+      inputs: [
+        { indexed: true, internalType: 'address', name: 'from', type: 'address' },
+        { indexed: true, internalType: 'address', name: 'to', type: 'address' },
+        { indexed: true, internalType: 'uint256', name: 'tokenId', type: 'uint256' }
+      ],
+      name: 'Transfer',
+      type: 'event'
+    },
+    {
+      inputs: [
+        { internalType: 'address', name: '', type: 'address' },
+        { internalType: 'uint256', name: '', type: 'uint256' }
+      ],
+      name: 'approve',
+      outputs: [],
+      stateMutability: 'pure',
+      type: 'function'
+    },
+    {
+      inputs: [{ internalType: 'address', name: 'owner', type: 'address' }],
+      name: 'balanceOf',
+      outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+      name: 'fileHash',
+      outputs: [{ internalType: 'bytes32', name: '', type: 'bytes32' }],
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+      name: 'getApproved',
+      outputs: [{ internalType: 'address', name: '', type: 'address' }],
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      inputs: [
+        { internalType: 'address', name: 'owner', type: 'address' },
+        { internalType: 'address', name: 'operator', type: 'address' }
+      ],
+      name: 'isApprovedForAll',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      inputs: [
+        { internalType: 'address', name: '_owner', type: 'address' },
+        { internalType: 'bytes32', name: '_fileHash', type: 'bytes32' },
+        { internalType: 'string', name: '_tokenURI', type: 'string' }
+      ],
+      name: 'mintCertificate',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function'
+    },
+    {
+      inputs: [],
+      name: 'name',
+      outputs: [{ internalType: 'string', name: '', type: 'string' }],
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      inputs: [],
+      name: 'nextTokenId',
+      outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      inputs: [],
+      name: 'owner',
+      outputs: [{ internalType: 'address', name: '', type: 'address' }],
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+      name: 'ownerOf',
+      outputs: [{ internalType: 'address', name: '', type: 'address' }],
+      stateMutability: 'view',
+      type: 'function'
+    },
+    { inputs: [], name: 'renounceOwnership', outputs: [], stateMutability: 'nonpayable', type: 'function' },
+    {
+      inputs: [
+        { internalType: 'address', name: 'from', type: 'address' },
+        { internalType: 'address', name: 'to', type: 'address' },
+        { internalType: 'uint256', name: 'tokenId', type: 'uint256' }
+      ],
+      name: 'safeTransferFrom',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function'
+    },
+    {
+      inputs: [
+        { internalType: 'address', name: '', type: 'address' },
+        { internalType: 'address', name: '', type: 'address' },
+        { internalType: 'uint256', name: '', type: 'uint256' },
+        { internalType: 'bytes', name: '', type: 'bytes' }
+      ],
+      name: 'safeTransferFrom',
+      outputs: [],
+      stateMutability: 'pure',
+      type: 'function'
+    },
+    {
+      inputs: [
+        { internalType: 'address', name: '', type: 'address' },
+        { internalType: 'bool', name: '', type: 'bool' }
+      ],
+      name: 'setApprovalForAll',
+      outputs: [],
+      stateMutability: 'pure',
+      type: 'function'
+    },
+    {
+      inputs: [{ internalType: 'bytes4', name: 'interfaceId', type: 'bytes4' }],
+      name: 'supportsInterface',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      inputs: [],
+      name: 'symbol',
+      outputs: [{ internalType: 'string', name: '', type: 'string' }],
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+      name: 'tokenURI',
+      outputs: [{ internalType: 'string', name: '', type: 'string' }],
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      inputs: [
+        { internalType: 'address', name: '', type: 'address' },
+        { internalType: 'address', name: '', type: 'address' },
+        { internalType: 'uint256', name: '', type: 'uint256' }
+      ],
+      name: 'transferFrom',
+      outputs: [],
+      stateMutability: 'pure',
+      type: 'function'
+    },
+    {
+      inputs: [{ internalType: 'address', name: 'newOwner', type: 'address' }],
+      name: 'transferOwnership',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function'
+    },
+    {
+      inputs: [
+        { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+        { internalType: 'bytes32', name: '_fileHash', type: 'bytes32' }
+      ],
+      name: 'verifyHash',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      stateMutability: 'view',
+      type: 'function'
+    }
+  ]
 
   const provider = new ethers.JsonRpcProvider(rpcUrl)
   const wallet = new ethers.Wallet(privateKey as string, provider)
   const contract = new ethers.Contract(contractAddress as string, contractABI, wallet)
 
-
   let receipt
-  try{
+  try {
     const tx = await contract.mintCertificate(owner, fileHashBytes32, metadataUrl)
-      receipt = await tx.wait()
-  }catch(err){
+    receipt = await tx.wait()
+  } catch (err) {
     throw new BadRequestError('Minting certificate failed!')
   }
-
 
   let tokenId: string | undefined
   for (const log of receipt.logs) {
